b267a8572bd937824c8f8bb7d23a214658ad5294
Hiroshi Urabe <mail@torounit.com>
2015-11-26 15:37:46 +0900

    init

---
+++ custom-field-column.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Custom-field-column
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: custom-field-column
+Domain Path: /languages
+*/
+
